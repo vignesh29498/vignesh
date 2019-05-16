6384b279a1d77cc6ca4be9b10286ab55c20dba39
vignesh29498 <50484461+vignesh29498@users.noreply.github.com>
2019-05-16 02:35:59 -0700

    Add files via upload

---
+++ 14a.rb
@@ -0,0 +1,16 @@
+n=ARGV.first 
+temp='>'
+puts " i ass some qes #{n}"
+puts " do you like "
+puts temp
+like= $stdin .gets.chomp
+
+puts " whats your age"
+puts temp
+age= $stdin .gets.chomp
+
+puts" what is your native"
+puts temp
+native=$stdin.gets.chomp
+
+puts"his name is #{n} and his age is #{age} and native pllace is #{native}"
