24f5e6a53c090c8cc1d5995d60b836da88e04e3c
vignesh29498 <50484461+vignesh29498@users.noreply.github.com>
2019-05-16 02:36:34 -0700

    Add files via upload

---
+++ 12.rb
@@ -0,0 +1,12 @@
+print"Enter a number"
+num=gets.chomp.to_i
+
+big=num*100
+puts " A biggest number is #{num}"
+
+print "enter another number"
+another=gets.chomp
+num=another.to_i
+
+small=num/100
+puts " A small number si #{small}"
---
+++ 13.rb
@@ -0,0 +1,17 @@
+first, second, third = ARGV
+
+puts "Your first variable is: #{first}"
+puts "Your second variable is: #{second}"
+puts "Your third variable is: #{third}"
+
+#vignesh@vignesh-Lenovo-B490:~$ ruby 13.rb 1 2 3
+# your output is 
+#Your first variable is: 1
+#Your second variable is: 2
+#Your third variable is: 3
+
+#ruby 13.rb aaa ccc gy 
+#Your first variable is: aaa
+#Your second variable is: ccc
+#Your third variable is: gy
+
